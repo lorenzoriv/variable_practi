c7925aef346fcd37a1b6695b52255ecd88ae1361
lorenzo rivera <117060307+lorenzoriv@users.noreply.github.com>
2022-11-02 21:05:53 -0700

    Update variables.rb

--- variables.rb
+++ variables.rb
@@ -1,11 +1,18 @@
-```ruby
 name = "lorenzo"
 pet_name = "aurora"
-pet_name # "bunny"
 age = 18
 grad_year = 2022
 first_paycheck = 864.35
 weight = 135.2
 male = true
-is_tired = ture
-```
+is_tired = true
+
+puts name
+puts age 
+puts weight
+name = "zo"
+age = 19
+weight = 132.5
+puts name
+puts age
+puts weight 
